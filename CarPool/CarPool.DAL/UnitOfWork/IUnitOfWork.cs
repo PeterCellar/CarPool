784ea9c7e2fbfd2cccb85cd6c5ca563d3a60cb16
PeterCellar <63493272+PeterCellar@users.noreply.github.com>
2022-06-11 22:48:54 +0200

    Add files via upload

---
+++ CarPool/CarPool.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using CarPool.DAL.Entities;
+
+namespace CarPool.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity;
+        Task CommitAsync();
+    }
+}
