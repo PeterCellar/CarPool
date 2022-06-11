8d487648eb673eb6498a8df031be774ac7f1eee4
PeterCellar <63493272+PeterCellar@users.noreply.github.com>
2022-06-11 22:45:11 +0200

    Add files via upload

---
+++ CarPool/CarPool.DAL/Factories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CarPool.DAL.Factories
+{
+    /// <summary>
+    /// EF Core CLI migration generation uses this DbContext to create model and migration
+    /// </summary>
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CarPoolDbContext>
+    {
+        public CarPoolDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<CarPoolDbContext> builder = new();
+            builder.UseSqlServer(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;
+                Initial Catalog = CarPool;
+                MultipleActiveResultSets = True;
+                Integrated Security = True; ");
+
+            return new CarPoolDbContext(builder.Options);
+        }
+    }
+}
