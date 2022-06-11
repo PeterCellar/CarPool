687c4be8121696f1b499c051d1d9f9c067415236
PeterCellar <63493272+PeterCellar@users.noreply.github.com>
2022-06-11 22:53:42 +0200

    Add files via upload

---
+++ CarPool/CarPool.DAL.Tests/TestsCommon/SqlLiteDbContextTestingFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CarPool.DAL.Tests;
+
+public class SqlLiteDbContextTestingFactory : IDbContextFactory<CarPoolDbContext>
+{
+    private readonly string _databaseName;
+    private readonly bool _seedTestingData;
+
+    public SqlLiteDbContextTestingFactory(string databaseName, bool seedTestingData = true)
+    {
+        _databaseName = databaseName;
+        _seedTestingData = seedTestingData;
+    }
+    public CarPoolDbContext CreateDbContext()
+    {
+        DbContextOptionsBuilder<CarPoolDbContext> builder = new();
+        builder.UseSqlite($"Data Source={_databaseName};Cache=Shared");
+
+        // contextOptionsBuilder.LogTo(System.Console.WriteLine); //Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
+        // builder.EnableSensitiveDataLogging();
+
+        return new CarPoolDbContext(builder.Options, _seedTestingData);
+    }
+}
