ad945c1ec27b72988e33b3ada8ef70bce18af0b0
PeterCellar <63493272+PeterCellar@users.noreply.github.com>
2022-06-11 22:47:32 +0200

    Add files via upload

---
+++ CarPool/CarPool.DAL/Seeds/NumberOfRidesSeed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CarPool.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarPool.DAL.Seeds
+{
+
+    public static class NumberOfRidesSeed
+    {
+        public static readonly NumberOfRidesEntity RideRelationship1 = new(
+            Id: Guid.Parse(input: "1422CA21-C6FF-413E-8070-FA8A896A1F8D"),
+            UserId: Guid.Parse(input: "AC3D462E-8DA6-46AB-B156-30688E44917A"),
+            RideId: Guid.Parse(input: "5D38C1DD-0CF1-42CD-925C-5E8DEA737A2A"))
+        {
+            User = UserSeed.Josef,
+            Ride = RideSeed.Ride1
+        };
+        public static readonly NumberOfRidesEntity RideRelationship2 = new(
+            Id: Guid.Parse(input: "B5BF3A8E-C312-4DDA-9D1F-9F6FD68E7C31"),
+            UserId: Guid.Parse(input: "CED8BFBB-93EE-49F2-A0C6-DF9B920E861A"),
+            RideId: Guid.Parse(input: "5D38C1DD-0CF1-42CD-925C-5E8DEA737A2A"))
+        {
+            User = UserSeed.Jakub,
+            Ride = RideSeed.Ride1
+        };
+
+
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NumberOfRidesEntity>().HasData(
+               RideRelationship1 with { User = null, Ride = null },
+               RideRelationship2 with { User = null, Ride = null }
+            );
+
+        }
+    }
+}
---
+++ CarPool/CarPool.DAL/Seeds/RideSeed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CarPool.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarPool.DAL.Seeds;
+
+public static class RideSeed
+{
+    public static readonly RideEntity Ride1 = new(
+        Id: Guid.Parse(input: "5D38C1DD-0CF1-42CD-925C-5E8DEA737A2A"),
+        StartTime: new DateTime(2023, 5, 5, 17, 0, 0),
+        EndTime: new DateTime(2023, 5, 5, 19, 0, 0),
+        StartLocation: "Brno",
+        EndLocation: "Praha",
+        UsedCarId: Guid.Parse(input: "3A8490B2-3BD3-410D-A513-6340699E9FA5"),
+        DriverId: Guid.Parse(input: "06a8a2cf-ea03-4095-a3e4-aa0291fe9c75")
+        );
+
+    public static readonly RideEntity Ride2 = new(
+       Id: Guid.Parse(input: "42042042-0CF1-42CD-925C-5E8DEA737A2A"),
+       StartTime: new DateTime(2023, 1, 1, 3, 0, 0),
+       EndTime: new DateTime(2023, 1, 1, 6, 0, 0),
+       StartLocation: "Ostrava",
+       EndLocation: "Bratislava",
+       UsedCarId: Guid.Parse(input: "3A8490B2-3BD3-410D-A513-6340699E9FA5"),
+       DriverId: Guid.Parse(input: "06a8a2cf-ea03-4095-a3e4-aa0291fe9c75")
+       );
+
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RideEntity>().HasData(
+            Ride1
+        );
+        modelBuilder.Entity<RideEntity>().HasData(
+            Ride2
+        );
+    }
+}
