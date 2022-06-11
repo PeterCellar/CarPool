cc939d66d81294d287fd1a250bc6573167d41727
PeterCellar <63493272+PeterCellar@users.noreply.github.com>
2022-06-11 22:22:58 +0200

    Create CarPoolDbContext.cs

---
+++ CarPool/CarPool.DAL/CarPoolDbContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using CarPool.DAL.Entities;
+using CarPool.DAL.Seeds;
+
+namespace CarPool.DAL
+{
+    // Access to database
+    public class CarPoolDbContext : DbContext
+    {
+        private readonly bool _seedDemoData;
+
+        public CarPoolDbContext(DbContextOptions contextOptions, bool seedDemoData = true)
+            : base(contextOptions)
+        {
+            _seedDemoData = seedDemoData;
+        }
+
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+        public DbSet<CarEntity> Cars => Set<CarEntity>();
+        public DbSet<NumberOfRidesEntity> NumberOfRides => Set<NumberOfRidesEntity>();
+        public DbSet<RideEntity> Rides => Set<RideEntity>();
+        
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+                UserSeed.Seed(modelBuilder);
+                CarSeed.Seed(modelBuilder);
+                RideSeed.Seed(modelBuilder);
+                NumberOfRidesSeed.Seed(modelBuilder);
+           
+        }
+    }
+}
